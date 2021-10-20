3ac0dc97d95de7a24b9505071f04879cbb775ebc
DrewH95 <andrew.j.hughes333@gmail.com>
2021-10-19 23:56:45 -0500

    Commit

---
+++ style.js
@@ -0,0 +1,16 @@
+let buttonplus = document.querySelector('#likes')
+
+var i=9
+
+function plus() {
+    i++
+    buttonplus.innerText=i + ' like(s)'
+}
+
+function plus1() {
+    document.querySelector("#likes2").innerHTML++;
+}
+
+function plus2() {
+    document.querySelector("#likes3").innerHTML++;
+}
